be52586a2265ef1b78b9bf0fc25d79877b03fd05
makaniski <makaniski@gmail.com>
2015-09-10 11:08:57 +0000

    M

--- zadaci/my/makaniski/ravnina.py
+++ zadaci/my/makaniski/ravnina.py
@@ -1,4 +1,5 @@
 
+
 from math import sqrt
 
 class Tocka:
@@ -17,18 +18,38 @@ class Tocka:
         return sqrt( self.x**2 + self.y**2  )
         
     
-    
     def __repr__(self):
         return "Tocka({0}, {1})".format(self.x,self.y)
         
 
 class Vektor(Tocka):
-    
+
     def __add__(self, b):
         """ self je vektor, b je vektor
         """
         x = self.x + b.x
         y = self.y + b.y
+        return Vektor(x, y)
+    
+    def __sub__(self, b):
+        """ self je vektor, b je vektor
+        """
+        x = self.x - b.x
+        y = self.y - b.y
+        return Vektor(x, y)
+    
+    def __mul__(self, b):
+        """ self je vektor, b je vektor
+        """
+        x = self.x * b
+        y = self.y * b
+        return Vektor(x, y)
+
+    def __rmul__(self, b):
+        """ self je vektor, b je vektor
+        """
+        x = b * self.x
+        y = b * self.y
         return Vektor(x, y)
     
     def __repr__(self):
