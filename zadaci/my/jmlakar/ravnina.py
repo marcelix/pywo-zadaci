fb1c13e17c679bd4128f48bd584623903ef76d98
jmlakar <josipmlakar2@gmail.com>
2015-09-18 18:27:00 +0200

    Commit zadace

--- zadaci/my/jmlakar/ravnina.py
+++ zadaci/my/jmlakar/ravnina.py
@@ -1,4 +1,3 @@
-import math
 class Tocka:
     """
         Opis ove klase:)
@@ -20,5 +19,24 @@ class Vektor (Tocka):
     def __add__(self, b):
         return ((self.x + b.x), (self.y + b.y))
     
+    def __sub__(self, b):
+        return ((self.x - b.x), (self.y - b.y))
+    
+    def __mul__(self, n):
+        return ((self.x * n), (self.y * n))
+    
+    def sc (self,b):
+        return self.x * b.x + self.y * b.y
+    
     def __repr__(self):
-        return "Tocka x={0}, y={1}".format(self.x,self.y)      
+        return "Tocka x={0}, y={1}".format(self.x,self.y) 
+    
+''' 
+a = Vektor(1,2)
+b = Vektor(-12,34)
+
+c = a * 2
+print(c)
+
+print (a.sc(b))   
+'''
